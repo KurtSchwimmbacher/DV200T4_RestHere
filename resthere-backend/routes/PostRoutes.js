d6ead064938facd3e2c112910a32eb093a38048c
KurtSchwimmbacher <231002@virtualwindow.co.za>
2024-10-11 15:50:42 +0200

    add get posts and display
    add backend route to get posts by a user and display them in front end

--- resthere-backend/routes/PostRoutes.js
+++ resthere-backend/routes/PostRoutes.js
@@ -2,6 +2,28 @@ const express = require('express');
 const Posts = require('../models/Posts');
 const router = express.Router();
 
+
+// get posts by user
+router.get('/user/:userId', async (req, res) => {
+    const { userId } = req.params;
+
+    try {
+        // Find all posts where the user matches the provided userId
+        const posts = await Posts.find({ user: userId }); 
+
+        if (!posts || posts.length === 0) {
+            return res.status(404).json({ message: 'No posts found for this user.' });
+        }
+
+        // Return the found posts
+        res.status(200).json(posts); 
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ msg: 'Server error' });
+    }
+});
+
+
 // new post route
 router.post('/create', async (req, res) => {
     const { title, content, user } = req.body;
@@ -27,5 +49,7 @@ router.post('/create', async (req, res) => {
         res.status(500).json({ msg: 'Server error' });
     }
 });
+
+
 
 module.exports = router;
--- resthere-frontend/src/components/PostContainer.js
+++ resthere-frontend/src/components/PostContainer.js
@@ -1,43 +1,70 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import PostCard from './PostCard';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import axios from 'axios';
+
+import { useDispatch, useSelector } from 'react-redux';
 
 const PostContainer = ({ handleShow }) => {
+
+    // get user info
+    const user = useSelector((state) => state.user);
+
+    // useState to hold user posts
+    const [userPosts, setUserPosts] = new useState([]);
+
+    
+
+    // function to fetch all posts by user
+    const fetchUserPosts = async (userId) => {
+        try {
+            const response = await axios.get(`http://localhost:5000/api/posts/user/${userId}`);
+            // store response data in useState
+            setUserPosts(response.data);
+        } catch (error) {
+            console.error('Error fetching user posts:', error);
+        }
+    };
+    
+    // use effect to ensure fetching re runs when user changes
+    // and doesnt run when user isnt signed in
+    useEffect(()=>{
+        if(user.userID){
+            // fetch posts by user
+            fetchUserPosts(user.userID);
+        }
+        // re run when userID changes
+    },[user.userID])
+
+
   return (
-    <Container>
-      <Row>
-        <Col>
-          <PostCard 
-            title="Post Card 1" 
-            text="Some description for the first chat card" 
-            handleShow={handleShow} 
-          />
-        </Col>
-        <Col>
-          <PostCard
-            title="Post Card 2" 
-            text="Some description for the second chat card" 
-            handleShow={handleShow} 
-          />
-        </Col>
-        <Col>
-          <PostCard
-            title="Post Card 3" 
-            text="Some description for the third chat card" 
-            handleShow={handleShow} 
-          />
-        </Col>
-        <Col>
-          <PostCard
-            title="Post Card 4" 
-            text="Some description for the fourth chat card" 
-            handleShow={handleShow} 
-          />
-        </Col>
-      </Row>
-    </Container>
+   <Container>
+    <Row>
+        {/* if posts exist */}
+        {userPosts.length > 0 ? (
+            // map each post
+            userPosts.map((post) => (
+                // ensure unique key for each column
+                <Col key={post._id}>
+                    {/* use data from db to populate card info */}
+                    <PostCard
+                        title={post.title}
+                        text={post.content}
+                        handleShow={handleShow}
+                    />
+                </Col>
+            ))
+        // rest of conditional statement
+        ):(
+            // message displayed if no posts exist
+            <Col>
+                <h4>No posts found</h4>
+            </Col>
+        )}
+    </Row>
+   </Container>
   );
 };
 
