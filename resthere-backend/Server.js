95fbb82aaf399aa24d893660b2af40a7e1ec405f
KurtSchwimmbacher <231002@virtualwindow.co.za>
2024-11-04 21:48:43 +0200

    update server

--- resthere-backend/Server.js
+++ resthere-backend/Server.js
@@ -54,14 +54,12 @@ app.use('/api/professional',professionalRoutes);
 app.use('/api/chat',chatRoutes);
 
 
-// Serve frontend build files in production
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'build')));  // Adjust this path if needed
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'build', 'index.html'));
-    });
-}
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
